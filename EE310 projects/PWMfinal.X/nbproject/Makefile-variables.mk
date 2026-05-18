9edeef9e9ab332bf227bdcf68adbf3a2f788d9a6
Goot-z <gutierrezstevie04@gmail.com>
2026-05-18 12:59:36 -0700

    Add files via upload

--- EE310 projects/PWMfinal.X/nbproject/Makefile-variables.mk
+++ EE310 projects/PWMfinal.X/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=PWMlab.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/PWMlab.X.production.hex
+CND_ARTIFACT_NAME_default=PWMfinal.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PWMfinal.X.production.hex
